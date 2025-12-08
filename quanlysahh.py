a0eced1d6aa8b1d36f6e4460e007f3e1fac4d973
nguyen thi thuy linh <nguyenthithuylinh070906@gmail.com>
2025-12-08 19:59:25 +0700

    feat:quanlysachh

---
+++ quanlysahh.py
@@ -0,0 +1,8 @@
+# ============================
+# CHƯƠNG TRÌNH QUẢN LÝ SÁCH
+# ============================
+
+# Mỗi quyển sách là 1 dict:
+# { "id": ..., "ten": ..., "tac_gia": ..., "nam": ..., "so_luong": ... }
+
+danh_sach_sach = []
